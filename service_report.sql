f7840003fc6d50c3c40cf2c6d01418ec89e47f1b
danieltk76 <121234750+danieltk76@users.noreply.github.com>
2025-04-18 19:39:47 -0400

    Update service_report.sql

--- service_report.sql
+++ service_report.sql
@@ -1,6 +1,4 @@
--- Service Reporting Application
 
--- Get all services for a specific customer
 SELECT s.serviceId, s.description, s.startDate, s.endDate, e.name as AssignedEmployee
 FROM Service s
 JOIN EmployeeAssignment ea ON s.serviceId = ea.serviceId
@@ -8,14 +6,13 @@ JOIN Employee e ON ea.employeeId = e.empId
 WHERE s.customerId = 1
 ORDER BY s.startDate;
 
--- Get order details including items
 SELECT o.orderId, o.orderDate, o.orderStatus, o.orderCost, 
        i.orderItemId, i.quantity, i.unitPrice
 FROM CustomerOrder o
 JOIN OrderItem i ON o.orderId = i.orderId
 WHERE o.customerId = 1;
 
--- Monthly revenue report
+-- Monthly rev
 SELECT 
     strftime('%Y-%m', orderDate) as Month,
     COUNT(*) as OrderCount,
